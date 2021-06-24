764997184151c1d63557f890f61a542109b70a61
johnny <johnnywevany@outlook.com>
2021-06-24 20:36:13 -0300

    adding

--- src/components/Member/index.tsx
+++ src/components/Member/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Avatar } from '../Avatar';
 
 import { Container } from './styles';
+
+type Props = {
+
+}
 
 export function Member() {
   return (
     <Container>
-
+      <Avatar
+        urlImage={ }
+      />
     </Container>
   )
 }
